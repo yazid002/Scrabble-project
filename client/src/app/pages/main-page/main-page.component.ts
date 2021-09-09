daea1e8823b1229ab6a6f1b68927547f2f5cd3d6
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-09 16:52:59 -0400

    restauré les méthodes dont on utilise pas encore
    Les méthodes getMessagesFromServer() et sendTimeToServer() ne sont pas encore utilisé mais sont dans les tests nécessaire pour approuver un merge request

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GameModeDialogComponent } from '@app/components/dialogs/game-mode-dialog/game-mode-dialog.component';
+import { Message } from '@app/classes/message';
+import { CommunicationService } from '@app/services/communication.service';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-main-page',
@@ -16,8 +19,28 @@ export class MainPageComponent {
 
     readonly title: string = 'LOG2990';
     message: BehaviorSubject<string> = new BehaviorSubject<string>('');
-    constructor(public dialog: MatDialog) {}
+    constructor(public dialog: MatDialog, private readonly communicationService: CommunicationService) {}
 
+    sendTimeToServer(): void {
+        const newTimeMessage: Message = {
+            title: 'Hello from the client',
+            body: 'Time is : ' + new Date().toString(),
+        };
+        // Important de ne pas oublier "subscribe" ou l'appel ne sera jamais lancé puisque personne l'observe
+        this.communicationService.basicPost(newTimeMessage).subscribe();
+    }
+
+    getMessagesFromServer(): void {
+        this.communicationService
+            .basicGet()
+            // Cette étape transforme l'objet Message en un seul string
+            .pipe(
+                map((message: Message) => {
+                    return `${message.title} ${message.body}`;
+                }),
+            )
+            .subscribe(this.message);
+    }
     openDialog(mode: string) {
         mode.replace('c', 'c');
         if (mode === this.userChoices.classic) {
