c5d51cb04d2404ea2100d5e91886ba2440d23b8e
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-12 07:09:10 -0400

    routing de la page leaderboard
    Le bouton 'meilleurs scores' de la main page redirige vers la page leaderboard

--- client/src/app/pages/leaderboard-page/leaderboard-page.component.spec.ts
+++ client/src/app/pages/leaderboard-page/leaderboard-page.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LeaderboardPageComponent } from './leaderboard-page.component';
 
 describe('LeaderboardPageComponent', () => {
-  let component: LeaderboardPageComponent;
-  let fixture: ComponentFixture<LeaderboardPageComponent>;
+    let component: LeaderboardPageComponent;
+    let fixture: ComponentFixture<LeaderboardPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LeaderboardPageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LeaderboardPageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LeaderboardPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LeaderboardPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/pages/leaderboard-page/leaderboard-page.component.ts
+++ client/src/app/pages/leaderboard-page/leaderboard-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-leaderboard-page',
-  templateUrl: './leaderboard-page.component.html',
-  styleUrls: ['./leaderboard-page.component.scss']
+    selector: 'app-leaderboard-page',
+    templateUrl: './leaderboard-page.component.html',
+    styleUrls: ['./leaderboard-page.component.scss'],
 })
-export class LeaderboardPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class LeaderboardPageComponent {}
