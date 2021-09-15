b26170b564703ca6f9ea73a8959d22e7d40248b3
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-15 17:24:55 -0400

    Ajout de la logique de base pour envoyer un message

--- client/src/app/components/chatbox/chatbox.component.ts
+++ client/src/app/components/chatbox/chatbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChatService } from '@app/chat.service';
+import {IChat, ME, HIM} from '../../chat'
 
 @Component({
     selector: 'app-chatbox',
@@ -10,19 +11,33 @@ import { ChatService } from '@app/chat.service';
 export class ChatboxComponent implements OnInit {
     myForm: FormGroup;
     minLength: number = 5;
-    maxLength:number = 25;
+    maxLength: number = 25;
+    messages: IChat[] = [];
+
+    me = ME;
+    him = HIM;
     constructor(public chatService: ChatService, private fb: FormBuilder) {}
 
     ngOnInit(): void {
         this.myForm = this.fb.group({
             message: ['', [Validators.required, Validators.minLength(this.minLength), Validators.maxLength(this.maxLength)]],
         });
-        this.myForm.valueChanges.subscribe(/*console.log*/);
+        this.myForm.valueChanges.subscribe(/* console.log*/);
     }
     get message() {
-        let message = this.myForm.get('message');
-        console.log("message min length:");
-        console.log(message?.errors?.minlength);
+        const message = this.myForm.get('message');
+        // console.log('message min length:');
+        // console.log(message?.errors?.minlength);
         return message;
+    }
+    getMessages(): void {
+        this.messages = this.chatService.getMessages();
+    }
+    onSubmit() {
+        console.log('starting submission');
+        console.warn(this.myForm.value);
+        this.chatService.addMessage(this.myForm.value.message);
+        this.getMessages();
+        this.myForm.reset();
     }
 }
