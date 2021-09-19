d26f6567670869a89747d5d310fdb935c58f9314
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-19 18:25:16 -0400

    lingting

--- client/src/app/components/chatbox/chatbox.component.ts
+++ client/src/app/components/chatbox/chatbox.component.ts
@@ -4,6 +4,7 @@ import { IChat, SENDER } from '@app/classes/chat';
 import { ChatService } from '@app/services/chat.service';
 import { CommandExecutionService } from '@app/services/command-execution/command-execution.service';
 
+const MAX_MESSAGE_LENGTH = 512;
 @Component({
     selector: 'app-chatbox',
     templateUrl: './chatbox.component.html',
@@ -12,13 +13,14 @@ import { CommandExecutionService } from '@app/services/command-execution/command
 export class ChatboxComponent implements OnInit {
     myForm: FormGroup;
     minLength: number = 0;
-    maxLength: number = 512;
+    maxLength: number = MAX_MESSAGE_LENGTH;
     messages: IChat[] = [];
     readonly possibleSenders = SENDER;
 
     constructor(public chatService: ChatService, private fb: FormBuilder, private commandExecutionService: CommandExecutionService) {}
 
     ngOnInit(): void {
+        // const placerPattern = Validators.pattern('^!placer[\\s][a-z]+[0-9]+(h|v)[\\s][A-Za-z]+$');
         this.myForm = this.fb.group({
             message: ['', [Validators.required, Validators.minLength(this.minLength), Validators.maxLength(this.maxLength)]],
         });
@@ -29,10 +31,8 @@ export class ChatboxComponent implements OnInit {
         const message = this.myForm.get('message');
         return message;
     }
-    getMessages(): void {
-        this.chatService.getMessages().subscribe((messages) => (this.messages = messages));
-    }
     onSubmit() {
+        if (!this.myForm.valid) return;
         const body = this.myForm.value.message;
         if (body.startsWith('!')) {
             const result: IChat = this.commandExecutionService.interpretCommand(body);
