b3c0d4df14f9fafa3a856c17c706b241f67164fc
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-23 10:05:25 -0400

    ajout de la validation

--- client/src/app/components/dialogs/game-mode-dialog/game-mode-dialog.component.ts
+++ client/src/app/components/dialogs/game-mode-dialog/game-mode-dialog.component.ts
@@ -9,5 +9,18 @@ import * as GAME_SETTINGS from '@app/classes/game-options';
 export class GameModeDialogComponent {
     readonly settings = GAME_SETTINGS.SETTINGS;
     readonly nameOption = GAME_SETTINGS.NAME_OPTION;
-    
+    name: string = '';
+    error: boolean;
+    errorMessage: string=''
+
+    validateName() {
+        console.log(this.name);
+        if (this.name === 'allo'){
+            this.error = false;
+            this.errorMessage = 'valide';
+        } else {
+            this.error = true;
+            this.errorMessage = 'cest pas allo';
+        }
+    }
 }
