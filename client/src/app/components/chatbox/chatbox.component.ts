b1e410250e6a5f761650d094272f38718d71fbf1
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-23 08:25:26 -0400

    Simplifier la logique d'application

--- client/src/app/components/chatbox/chatbox.component.ts
+++ client/src/app/components/chatbox/chatbox.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { IChat, SENDER } from '@app/classes/chat';
 import { ChatService } from '@app/services/chat.service';
 import { CommandExecutionService } from '@app/services/command-execution/command-execution.service';
@@ -11,38 +11,31 @@ const MAX_MESSAGE_LENGTH = 512;
     styleUrls: ['./chatbox.component.scss'],
 })
 export class ChatboxComponent implements OnInit {
-    myForm: FormGroup;
+    inputBox: string='';
     minLength: number = 0;
     maxLength: number = MAX_MESSAGE_LENGTH;
     messages: IChat[] = [];
     readonly possibleSenders = SENDER;
 
-    constructor(public chatService: ChatService, private fb: FormBuilder, private commandExecutionService: CommandExecutionService) {}
+    constructor(public chatService: ChatService, private commandExecutionService: CommandExecutionService) {}
 
     ngOnInit(): void {
         // const placerPattern = Validators.pattern('^!placer[\\s][a-z]+[0-9]+(h|v)[\\s][A-Za-z]+$');
-        this.myForm = this.fb.group({
-            message: ['', [Validators.required, Validators.minLength(this.minLength), Validators.maxLength(this.maxLength)]],
-        });
-        this.myForm.valueChanges.subscribe();
+        
         this.getMessages();
     }
-    get message() {
-        const message = this.myForm.get('message');
-        return message;
-    }
+    
     onSubmit() {
-        if (!this.myForm.valid) return;
-        const body = this.myForm.value.message;
-        const message: IChat = { from: this.possibleSenders.me, body };
+        
+        const message: IChat = { from: this.possibleSenders.me, body: this.inputBox };
         this.chatService.addMessage(message);
-        if (body.startsWith('!')) {
-            const result: IChat = this.commandExecutionService.interpretCommand(body);
+        if (this.inputBox.startsWith('!')) {
+            const result: IChat = this.commandExecutionService.interpretCommand(this.inputBox);
 
             this.chatService.addMessage(result);
         }
         // this.getMessages();
-        this.myForm.reset();
+        
         this.scrollDown();
     }
     private scrollDown() {
