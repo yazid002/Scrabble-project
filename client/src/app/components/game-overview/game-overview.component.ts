6ac60787854ca12cb63916eaf3fcd5d6d9545a1c
Maxime Laroche <mlaroche2009@gmail.com>
2021-09-27 08:48:35 -0400

    Affiche maintenant le temps par tour dans le sidebar

--- client/src/app/components/game-overview/game-overview.component.ts
+++ client/src/app/components/game-overview/game-overview.component.ts
@@ -10,6 +10,7 @@ export class GameOverviewComponent implements OnInit {
     mode: string;
     numPlayers: string;
     computerLevel: string;
+    timer: string;
     constructor(public userSettingsService: UserSettingsService) {}
     ngOnInit(): void {
         console.log('In the game overview');
@@ -22,11 +23,17 @@ export class GameOverviewComponent implements OnInit {
         const computerLevel = this.userSettingsService.settings.computerLevel.setting.availableChoices.find(
             (key) => key.key === this.userSettingsService.settings.computerLevel.currentChoiceKey,
         );
+
+        const timer = this.userSettingsService.settings.timer.setting.availableChoices.find(
+            (key) => key.key === this.userSettingsService.settings.timer.currentChoiceKey,
+        );
         console.log(mode, numPlayers, computerLevel);
-        if (mode && numPlayers && computerLevel) {
+
+        if (mode && numPlayers && computerLevel && timer) {
             this.mode = mode.value;
             this.numPlayers = numPlayers.value;
             this.computerLevel = computerLevel.value;
+            this.timer = timer.value;
         }
     }
 }
