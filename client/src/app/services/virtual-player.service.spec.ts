9b0b08400eacb1426675c3127d5fb2086c058b26
Maxime Laroche <mlaroche2009@gmail.com>
2021-10-12 11:47:47 -0400

    Echanger

--- client/src/app/services/virtual-player.service.spec.ts
+++ client/src/app/services/virtual-player.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { VirtualPlayerService } from './virtual-player.service';
 
 describe('VirtualPlayerService', () => {
-  let service: VirtualPlayerService;
+    let service: VirtualPlayerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VirtualPlayerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(VirtualPlayerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
