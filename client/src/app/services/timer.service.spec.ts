a7b96e6ee56be02b8ce8727ad2b281e0a7e03f29
Maxime Laroche <mlaroche2009@gmail.com>
2021-10-12 12:58:00 -0400

    100% coverage timerService

--- client/src/app/services/timer.service.spec.ts
+++ client/src/app/services/timer.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { TimerService } from './timer.service';
 
-describe('TimerService', () => {
+fdescribe('TimerService', () => {
     let service: TimerService;
 
     beforeEach(() => {
@@ -11,5 +11,16 @@ describe('TimerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+    it('should emit an event when timer hits zero', () => {
+        let value = false;
+        service.timerDone.next(value);
+        service.counter.totalTimer = service.counter.resetValue - 1;
+        service.timerDone.subscribe((val) => {
+            value = val;
+        });
+        // eslint-disable-next-line dot-notation
+        service['decrementTime']();
+        expect(value).toBe(true);
     });
 });
