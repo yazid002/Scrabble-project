936f95cffa7f1be10f528c325c1440109bb8ac92
Maxime Laroche <mlaroche2009@gmail.com>
2021-10-30 10:36:23 -0400

    avancé sur les tests gameService

--- client/src/app/services/game.service.spec.ts
+++ client/src/app/services/game.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GameService } from './game.service';
+import { GameService, OTHER_PLAYER } from './game.service';
 
-describe('GameService', () => {
+fdescribe('GameService', () => {
     let service: GameService;
 
     beforeEach(() => {
@@ -12,5 +12,25 @@ describe('GameService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+    it("should emit tell virtualPlayerService it is his turn to play if it's the other player's turn\
+    and we're converting the game from multiplayer to solo", () => {
+        service.currentTurn = OTHER_PLAYER;
+        const spy = spyOn(service.otherPlayerSignal, 'next');
+        service.numPlayers = 'multiplayer';
+        service.convertGameToSolo();
+        expect(spy).toHaveBeenCalled();
+    });
+    it('should emit an abandon signal when we quit the game', () => {
+        const spy = spyOn(service.abandonSignal, 'next');
+        service.quitGame();
+        expect(spy).toHaveBeenCalled();
+    });
+    it('should reset the skipcounter when the turn when the last move was not a skip', () => {
+        service.skipCounter = 4;
+        // eslint-disable-next-line dot-notation
+        service['changeTurn'](false);
+        const expectedValue = 0;
+        expect(service.skipCounter).toEqual(expectedValue);
     });
 });
