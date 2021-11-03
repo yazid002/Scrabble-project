31b39ffbcba492e03edba301b33ad605dd0d12f5
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-03 08:58:57 -0400

    Tests SwitchDialogComponent

--- client/src/app/components/switch-dialog/switch-dialog.component.spec.ts
+++ client/src/app/components/switch-dialog/switch-dialog.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NAME_OPTION } from '@app/classes/game-options';
 import { UserSettingsService } from '@app/services/user-settings.service';
+import { of } from 'rxjs';
 import { SwitchDialogComponent } from './switch-dialog.component';
 
+class MatDialogMock {
+    open() {
+        return {
+            afterClosed: () => of({}),
+        };
+    }
+}
 describe('SwitchDialogComponent', () => {
     let component: SwitchDialogComponent;
     let fixture: ComponentFixture<SwitchDialogComponent>;
@@ -21,10 +29,13 @@ describe('SwitchDialogComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [SwitchDialogComponent],
             providers: [
-                { provide: MAT_DIALOG_DATA, useValue: {} },
+                {
+                    provide: MatDialog,
+                    useClass: MatDialogMock,
+                },
                 { provide: UserSettingsService, useValue: userSettingsServiceSpy },
             ],
-            imports: [BrowserAnimationsModule, MatRadioModule, MatCardModule, FormsModule, MatInputModule],
+            imports: [BrowserAnimationsModule, MatRadioModule, MatCardModule, FormsModule, MatInputModule, MatDialogModule],
         }).compileComponents();
     });
 
