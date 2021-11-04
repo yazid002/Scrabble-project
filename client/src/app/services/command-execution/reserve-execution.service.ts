6d4d83d5643cd1321b4f081768826604497325a6
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-04 13:20:16 -0400

    plaus de throw dans command-execution et chatBox

--- client/src/app/services/command-execution/reserve-execution.service.ts
+++ client/src/app/services/command-execution/reserve-execution.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@angular/core';
 import { IChat, SENDER } from '@app/classes/chat';
 import { ReserveService } from '@app/services/reserve.service';
-import { DebugExecutionService } from './debug-execution.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ReserveExecutionService {
-    constructor(private reserveService: ReserveService, private debugExecutionService: DebugExecutionService) {}
+    constructor(private reserveService: ReserveService) {}
 
-    execute(): IChat {
-        let result: IChat = {
-            from: SENDER.computer,
-            body: 'La commande <strong>reserve</strong> est uniquement disponible lorsque le mode débogage est activé',
-        };
-        if (this.debugExecutionService.state) {
-            result = this.buildReserveMessage();
-        }
-
-        return result;
-    }
-    private buildReserveMessage(): IChat {
+    execute() {
         const result: IChat = {
             from: SENDER.computer,
             body: '',
--- client/src/app/services/room.service.ts
+++ client/src/app/services/room.service.ts
@@ -29,7 +29,8 @@ export class RoomService {
         private gameService: GameService,
         private userSettingsService: UserSettingsService,
     ) {
-        this.urlString = 'ec2-99-79-57-8.ca-central-1.compute.amazonaws.com:3000'; // environment.serverUrl;
+        this.urlString = 'ec2-99-79-57-8.ca-central-1.compute.amazonaws.com:3000';
+        this.urlString = '127.0.0.1:3000';
         this.socket = io(this.urlString);
         this.configureRoomCommunication();
         this.chatServiceSubscription = this.chatService.messageSent.subscribe((message: string) => {
