2035ce8c4078e0b10e2816bef343307eaa85699a
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-04 13:21:03 -0400

    meme que dernier commit

--- client/src/app/services/command-execution/reserve-execution.service.spec.ts
+++ client/src/app/services/command-execution/reserve-execution.service.spec.ts
@@ -1,13 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { IChat, SENDER } from '@app/classes/chat';
+import { SENDER } from '@app/classes/chat';
 import { ReserveExecutionService } from '@app/services/command-execution/reserve-execution.service';
 import { ReserveService } from '@app/services/reserve.service';
-import { DebugExecutionService } from './debug-execution.service';
 
 describe('ReserveExecutionService', () => {
     let service: ReserveExecutionService;
     let reserveServiceSpy: jasmine.SpyObj<ReserveService>;
-    let debugExecutionServiceSpy: jasmine.SpyObj<DebugExecutionService>;
 
     beforeEach(() => {
         reserveServiceSpy = jasmine.createSpyObj('ReserveService', ['getQuantityOfAvailableLetters', 'getLettersFromReserve', 'addLetterInReserve']);
@@ -17,13 +15,8 @@ describe('ReserveExecutionService', () => {
             { name: 'C', quantity: 2, points: 3, affiche: 'C' },
         ];
 
-        debugExecutionServiceSpy = jasmine.createSpyObj('DebugExecutionService', ['execute']);
-
         TestBed.configureTestingModule({
-            providers: [
-                { provide: ReserveService, useValue: reserveServiceSpy },
-                { provide: DebugExecutionService, useValue: debugExecutionServiceSpy },
-            ],
+            providers: [{ provide: ReserveService, useValue: reserveServiceSpy }],
         });
         service = TestBed.inject(ReserveExecutionService);
     });
@@ -33,28 +26,12 @@ describe('ReserveExecutionService', () => {
     });
 
     describe('execute', () => {
-        it('not display reserve if debug mode is inactive', () => {
+        it('should return an IChat instance with one line per letter in the body', () => {
             const result = service.execute();
-
-            const expectedResult = 'La commande <strong>reserve</strong> est uniquement disponible lorsque le mode débogage est activé';
-
-            expect(result.body).toEqual(expectedResult);
-        });
-
-        it('should return the right result if debugExecutionServiceSpy.state is true', () => {
-            debugExecutionServiceSpy.state = true;
-            const result = service.execute();
-
-            const expectedResult: IChat = {
-                from: SENDER.computer,
-                body: 'A: 9<br>B: 2<br>C: 2<br>',
-            };
-
-            // Car buildReserveMessage est privée
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            spyOn<any>(service, 'buildReserveMessage').and.returnValue(expectedResult);
-
-            expect(result.body).toEqual(expectedResult.body);
+            const numletters = result.body.split('<br>').length - 1;
+            const numLettersExpected = 3;
+            expect(result.from).toEqual(SENDER.computer);
+            expect(numletters).toEqual(numLettersExpected);
         });
     });
 });
