fbfec2a78a70cd4955d64de51ce1fa96db8f56ac
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-13 14:16:38 -0500

    ajout du dialog indiquant le mode de jeux

---
+++ client/src/app/components/opponent-quit-dialog/opponent-quit-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opponent-quit-dialog',
+  templateUrl: './opponent-quit-dialog.component.html',
+  styleUrls: ['./opponent-quit-dialog.component.scss']
+})
+export class OpponentQuitDialogComponent {
+
+  constructor() { }
+}
