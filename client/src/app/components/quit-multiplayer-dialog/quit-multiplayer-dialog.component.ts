a4728af4bd7c00bf65999fbb0c730c0a068886b1
Yazid Ben said <yazid.bensaid07@gmail.com>
2021-11-24 18:35:46 -0500

    rearrangemt des boutons du main-page done

--- client/src/app/components/quit-multiplayer-dialog/quit-multiplayer-dialog.component.ts
+++ client/src/app/components/quit-multiplayer-dialog/quit-multiplayer-dialog.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RoomService } from '@app/services/room.service';
-
+import { SoundManagerService } from '@app/services/sound-manager.service';
 @Component({
     selector: 'app-quit-multiplayer-dialog',
     templateUrl: './quit-multiplayer-dialog.component.html',
     styleUrls: ['./quit-multiplayer-dialog.component.scss'],
 })
 export class QuitMultiplayerDialogComponent {
-    constructor(private roomService: RoomService) {}
+    constructor(private roomService: RoomService, public soundManagerService: SoundManagerService) {}
 
     quitRoom() {
         this.roomService.quitRoom();
+    }
+
+    playClickOnButtonAudio() {
+        this.soundManagerService.playClickOnButtonAudio();
     }
 }
