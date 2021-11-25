e6a9c0ce5ebb6c802329cc66f3621bfc5c19ee4c
Ouleymatou Rakieta Ouedraogo <ouleymatou-r-m-elsa.ouedraogo@polymtl.ca>
2021-11-24 19:24:37 -0500

    Ajout du goal service spy comme providers du game overview component

--- client/src/app/services/leaderboard.service.spec.ts
+++ client/src/app/services/leaderboard.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LeaderboardService } from './leaderboard.service';
 
 describe('LeaderboardService', () => {
-  let service: LeaderboardService;
+    let service: LeaderboardService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LeaderboardService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(LeaderboardService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
