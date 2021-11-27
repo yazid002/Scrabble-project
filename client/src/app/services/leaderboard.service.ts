59de990e9971a2d4a6026c55c818558ac8fac97b
unknown <alex.hua@polymtl.ca>
2021-11-27 15:53:52 -0500

    switch des leaderboard classic et mode2990 dans le visuel, et ajout de reset lorsqu'il y a moins que 5 player dans le leaderboard

--- client/src/app/services/leaderboard.service.ts
+++ client/src/app/services/leaderboard.service.ts
@@ -20,13 +20,13 @@ export class LeaderboardService {
     }
 
     async fetchClassic() {
-        const getClassicUrl = this.urlString;
+        const getClassicUrl = this.urlString + '/ClassicLeaderboard';
         await this.http.get<Leaderboard[]>(getClassicUrl).subscribe((data) => {
             this.leaderboardClassic = this.sortLeaderBoard(data);
         });
     }
     async fetchLog2990() {
-        const getlog2990Url = this.urlString + '/ClassicLeaderboard';
+        const getlog2990Url = this.urlString;
         await this.http.get<Leaderboard[]>(getlog2990Url).subscribe((data) => {
             this.leaderboardMode2990 = this.sortLeaderBoard(data);
         });
@@ -35,16 +35,16 @@ export class LeaderboardService {
         return data.sort((a: Leaderboard, b: Leaderboard) => b.score - a.score);
     }
 
-    async addPlayer(player: Leaderboard) {
-        console.log('bbb');
-        console.log(this.urlString);
-        console.log(player);
-        this.http.post<Leaderboard>(this.urlString, player).subscribe();
-    }
+    // async addPlayer(player: Leaderboard) {
+    //     console.log('bbb');
+    //     console.log(this.urlString);
+    //     console.log(player);
+    //     this.http.post<Leaderboard>(this.urlString, player).subscribe();
+    // }
 
-    async deletePlayer(name: string) {
-        const url = `${this.urlString}/${name}`;
-        console.log(url);
-        return this.http.delete(url).subscribe();
-    }
+    // async deletePlayer(name: string) {
+    //     const url = `${this.urlString}/${name}`;
+    //     console.log(url);
+    //     return this.http.delete(url).subscribe();
+    // }
 }
--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -12,6 +12,7 @@ const DATABASE_URL =
 const DATABASE_NAME = 'Leaderboard';
 const DATABASE_COLLECTION = 'Mode 2990';
 const DATABASE_COLLECTION_CLASSIC = 'Classic';
+const DATABASE_MAX_VALUE = 5;
 export const DATABASE_VIRTUAL_NAMES = 'VirtualPlayerNames';
 
 @Service()
@@ -32,10 +33,12 @@ export class DatabaseService {
         } catch {
             throw new Error('Database connection error');
         }
-        if ((await this.db.collection(DATABASE_COLLECTION).countDocuments()) === 0) {
+        if ((await this.db.collection(DATABASE_COLLECTION).countDocuments()) !== DATABASE_MAX_VALUE) {
+            await this.db.collection(DATABASE_COLLECTION).deleteMany({});
             await this.populateMode2990LeaderBoard();
         }
-        if ((await this.db.collection(DATABASE_COLLECTION_CLASSIC).countDocuments()) === 0) {
+        if ((await this.db.collection(DATABASE_COLLECTION_CLASSIC).countDocuments()) !== DATABASE_MAX_VALUE) {
+            await this.db.collection(DATABASE_COLLECTION_CLASSIC).deleteMany({});
             await this.populateClassicLeaderBoard();
         }
 
