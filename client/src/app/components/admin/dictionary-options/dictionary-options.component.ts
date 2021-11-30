b758a1aaa3c831710d4b02fcfbfe2d270348113a
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-30 16:52:07 -0500

    transféré logique vers un service

--- client/src/app/components/admin/dictionary-options/dictionary-options.component.ts
+++ client/src/app/components/admin/dictionary-options/dictionary-options.component.ts
@@ -1,16 +1,12 @@
 /* eslint-disable arrow-parens */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { SERVER_URL } from '@app/constants/url';
-import { DictionaryModel } from '@app/pages/admin-page/models/dictionary.model';
 import { FileMessages } from '@app/pages/admin-page/models/file-messages.model';
 import { TitleDescriptionOfDictionary } from '@app/pages/admin-page/models/titleDescriptionOfDictionary.model';
 import { ValidationMessageModel } from '@app/pages/admin-page/models/validation-message.model';
 import { DictionaryService } from '@app/services/admin/dictionary.service';
-
 
 @Component({
     selector: 'app-dictionary-options',
@@ -33,11 +29,11 @@ export class DictionaryOptionsComponent implements OnInit {
         message: '',
     };
     myInputVariable: ElementRef;
-    constructor(
-        public dictionaryService: DictionaryService,
-        private http: HttpClient,
-        private snackBar: MatSnackBar,
-    ) {}
+    constructor(public dictionaryService: DictionaryService, private snackBar: MatSnackBar) {
+        this.dictionaryService.snackBarSignal.subscribe((snack: { message: string; action: string }) => {
+            this.openSnackBar(snack.message, snack.action);
+        });
+    }
 
     async ngOnInit(): Promise<void> {
         await this.dictionaryService.getAllDictionaries();
@@ -45,79 +41,9 @@ export class DictionaryOptionsComponent implements OnInit {
 
     async selectDictionary(event: any) {
         if (event.target.files.length > 0) {
-            this.newDictionary = event.target.files[0]; // file;
-            this.titleAndDescriptionOfDictionary.filename = this.newDictionary.name;
-            const reader = new FileReader();
-            reader.readAsText(this.newDictionary);
-            reader.onload = () => {
-                const jsonResp = JSON.stringify(reader.result);
-                try {
-                    const rep = JSON.parse(jsonResp);
-                    const newDic: DictionaryModel = JSON.parse(rep);
-                    this.titleAndDescriptionOfDictionary.title = newDic.title;
-                    this.titleAndDescriptionOfDictionary.description = newDic.description;
-                    if (Object.keys(newDic).length !== 3) {
-                        this.validationMessage.isValid = false;
-                        this.validationMessage.message = 'Lefichier doit avoir seulement 3 cles: title,description,words';
-                        this.openSnackBar(this.validationMessage.message, 'Dismiss');
-                    }
-                } catch (e) {
-                    this.validationMessage.isValid = false;
-                    this.validationMessage.message = 'le format du fichier doit etre json';
-                    this.openSnackBar(this.validationMessage.message, 'Dismiss');
-                }
-                if (this.dictionaryService.isNewDictionaryHasSameTitleAsAnother()) {
-                    this.validationMessage.isValid = false;
-                    this.validationMessage.message = "le dictionnaire a le meme titre q'un autre dictionnaire, svp change le titre !!!";
-                    this.openSnackBar(this.validationMessage.message, 'Dismiss');
-                }
-                this.validationMessage.isValid = true;
-                this.validationMessage.message = 'Le format du fichier est conforme au format attendu';
-            };
-            this.onSubmit();
+            const file = event.target.files[0];
+            this.dictionaryService.selectDictionary(file);
         }
-    }
-
-
-
-    async onSubmit() {
-        const file = new FormData();
-        file.set('file', this.newDictionary);
-        await this.http
-            .post(SERVER_URL + '/api/admin/dictionary/addNewDictionary', file)
-            .toPromise()
-            .then(
-                (resp: any) => {
-                    this.fileMessage.isuploaded = resp.isuploaded;
-                    this.fileMessage.message = resp.message;
-                },
-                (err) => {
-                    console.log('erreur : ' + JSON.stringify(err));
-                    this.openSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
-                },
-            );
-        if (!this.fileMessage.isuploaded) {
-            this.openSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
-        } else {
-            const statusUploading: FileMessages = await this.saveTitleAndDescription();
-            if (statusUploading.isuploaded) {
-                this.openSnackBar('Le dictionnaire a ete televerse avec success', 'Dismiss');
-                await this.dictionaryService.getAllDictionaries();
-            } else {
-                this.openSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
-            }
-        }
-    }
-
-    async saveTitleAndDescription(): Promise<FileMessages> {
-        await this.http
-            .post(SERVER_URL + '/api/admin/dictionary/addTitleAndDescription', this.titleAndDescriptionOfDictionary)
-            .toPromise()
-            .then((res: any) => {
-                this.fileMessage.isuploaded = res.isuploaded;
-                this.fileMessage.message = res.message;
-            });
-        return this.fileMessage;
     }
 
     openSnackBar(message: any, action: any) {
@@ -127,7 +53,6 @@ export class DictionaryOptionsComponent implements OnInit {
             snackBarRef.dismiss();
         });
     }
-
 
     updateDictionary(dictionary: TitleDescriptionOfDictionary) {
         // TODO a implementer
--- client/src/app/services/admin/dictionary.service.ts
+++ client/src/app/services/admin/dictionary.service.ts
@@ -4,19 +4,33 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable prettier/prettier */
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Output } from '@angular/core';
 import { SERVER_URL } from '@app/constants/url';
+import { DictionaryModel } from '@app/pages/admin-page/models/dictionary.model';
+import { FileMessages } from '@app/pages/admin-page/models/file-messages.model';
 import { TitleDescriptionOfDictionary } from '@app/pages/admin-page/models/titleDescriptionOfDictionary.model';
+import { ValidationMessageModel } from '@app/pages/admin-page/models/validation-message.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DictionaryService {
+    @Output() snackBarSignal = new BehaviorSubject<{ message: string, action: string }>({ message:'',action: ''});
+
     listDictionaries: TitleDescriptionOfDictionary[] = [];
     titleAndDescriptionOfDictionary: TitleDescriptionOfDictionary = {
         filename: '',
         title: '',
         description: '',
+    };
+    validationMessage: ValidationMessageModel = {
+        isValid: true,
+        message: '',
+    };
+    fileMessage: FileMessages = {
+        isuploaded: true,
+        message: '',
     };
     url: string;
     constructor(private http: HttpClient) {
@@ -47,5 +61,78 @@ export class DictionaryService {
             console.log(rep);
             this.getAllDictionaries();
         });
+    }
+    async selectDictionary(file: File) {
+        this.titleAndDescriptionOfDictionary.filename = file.name;
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => {
+            const jsonResp = JSON.stringify(reader.result);
+            try {
+                const rep = JSON.parse(jsonResp);
+                const newDic: DictionaryModel = JSON.parse(rep);
+                this.titleAndDescriptionOfDictionary.title = newDic.title;
+                this.titleAndDescriptionOfDictionary.description = newDic.description;
+                if (Object.keys(newDic).length !== 3) {
+                    this.validationMessage.isValid = false;
+                    this.validationMessage.message = 'Lefichier doit avoir seulement 3 cles: title,description,words';
+                    this.emitToSnackBar(this.validationMessage.message, 'Dismiss');
+                }
+            } catch (e) {
+                this.validationMessage.isValid = false;
+                this.validationMessage.message = 'le format du fichier doit etre json';
+                this.emitToSnackBar(this.validationMessage.message, 'Dismiss');
+            }
+            if (this.isNewDictionaryHasSameTitleAsAnother()) {
+                this.validationMessage.isValid = false;
+                this.validationMessage.message = "le dictionnaire a le meme titre q'un autre dictionnaire, svp change le titre !!!";
+                this.emitToSnackBar(this.validationMessage.message, 'Dismiss');
+            }
+            this.validationMessage.isValid = true;
+            this.validationMessage.message = 'Le format du fichier est conforme au format attendu';
+        };
+        this.upload(file);
+    }
+    async upload(file: File) {
+        const fileForm = new FormData();
+        fileForm.set('file', file);
+        await this.http
+            .post(SERVER_URL + '/api/admin/dictionary/addNewDictionary', fileForm)
+            .toPromise()
+            .then(
+                (resp: any) => {
+                    this.fileMessage.isuploaded = resp.isuploaded;
+                    this.fileMessage.message = resp.message;
+                },
+                (err) => {
+                    console.log('erreur : ' + JSON.stringify(err));
+                    this.emitToSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
+                },
+            );
+        if (!this.fileMessage.isuploaded) {
+            this.emitToSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
+        } else {
+            const statusUploading: FileMessages = await this.saveTitleAndDescription();
+            if (statusUploading.isuploaded) {
+                this.emitToSnackBar('Le dictionnaire a ete televerse avec success', 'Dismiss');
+                await this.getAllDictionaries();
+            } else {
+                this.emitToSnackBar('Probleme de televersement du fichier cote serveur', 'Dismiss');
+            }
+        }
+    }
+
+    async saveTitleAndDescription(): Promise<FileMessages> {
+        await this.http
+            .post(SERVER_URL + '/api/admin/dictionary/addTitleAndDescription', this.titleAndDescriptionOfDictionary)
+            .toPromise()
+            .then((res: any) => {
+                this.fileMessage.isuploaded = res.isuploaded;
+                this.fileMessage.message = res.message;
+            });
+        return this.fileMessage;
+    }
+    private emitToSnackBar(message: string, action: string) {
+        this.snackBarSignal.next({message,action})
     }
 }
