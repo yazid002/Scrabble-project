7289b9248312deefb36f220a10067490f7ceb588
Maxime Laroche <mlaroche2009@gmail.com>
2021-11-30 16:15:52 -0500

    enlevé redondance

--- client/src/app/components/admin/dictionary-options/dictionary-options.component.ts
+++ client/src/app/components/admin/dictionary-options/dictionary-options.component.ts
@@ -3,7 +3,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DictionaryModel } from '@app/pages/admin-page/models/dictionary.model';
 import { FileMessages } from '@app/pages/admin-page/models/file-messages.model';
@@ -11,8 +10,6 @@ import { TitleDescriptionOfDictionary } from '@app/pages/admin-page/models/title
 import { ValidationMessageModel } from '@app/pages/admin-page/models/validation-message.model';
 import { DictionaryService } from '@app/services/admin/dictionary.service';
 
-// import { FileMessages } from '../../../pages/admin-page/models/file-messages.model';
-// import { ValidationMessageModel } from './../../../pages/admin-page/models/validation-message.model';
 
 @Component({
     selector: 'app-dictionary-options',
@@ -20,7 +17,6 @@ import { DictionaryService } from '@app/services/admin/dictionary.service';
     styleUrls: ['./dictionary-options.component.scss'],
 })
 export class DictionaryOptionsComponent implements OnInit {
-    listDictionaries: TitleDescriptionOfDictionary[] = [];
     titleAndDescriptionOfDictionary: TitleDescriptionOfDictionary = {
         filename: '',
         title: '',
@@ -35,23 +31,15 @@ export class DictionaryOptionsComponent implements OnInit {
         isuploaded: true,
         message: '',
     };
-    matDialogRef: any;
     myInputVariable: ElementRef;
     constructor(
         public dictionaryService: DictionaryService,
-        public matDialog: MatDialog,
         private http: HttpClient,
         private snackBarService: MatSnackBar,
     ) {}
 
     async ngOnInit(): Promise<void> {
-        await this.getAllDictionaries();
-    }
-
-    async getAllDictionaries() {
-        this.listDictionaries.length = 0;
-        const res: any = await this.dictionaryService.getAllDictionaries();
-        this.listDictionaries = res;
+        await this.dictionaryService.getAllDictionaries();
     }
 
     async selectDictionary(event: any) {
@@ -91,7 +79,7 @@ export class DictionaryOptionsComponent implements OnInit {
 
     isNewDictionaryHasSameTitleAsAnother(): boolean {
         let result = false;
-        this.listDictionaries.forEach((dic): false | any => {
+        this.dictionaryService.listDictionaries.forEach((dic) => {
             if (dic.title.toLocaleLowerCase() === this.titleAndDescriptionOfDictionary.title.toLocaleLowerCase()) {
                 result = true;
             }
@@ -139,30 +127,15 @@ export class DictionaryOptionsComponent implements OnInit {
         return this.fileMessage;
     }
 
-    resetForm() {
-        console.log(this.myInputVariable.nativeElement.files);
-        this.myInputVariable.nativeElement.value = '';
-        console.log(this.myInputVariable.nativeElement.files);
-    }
-
     openSnackBar(message: any, action: any) {
         const snackBarRef = this.snackBarService.open(message, action, { duration: 5000 });
-        snackBarRef.afterDismissed().subscribe(() => {
-            this.resetForm();
-            console.log('---> the snackBar was dismissed');
-        });
         snackBarRef.onAction().subscribe(() => {
             console.log('---> the snackBar action was triggered');
+            snackBarRef.dismiss();
         });
     }
 
-    addDictionary() {
-        console.log('addDictionary')
-    }
 
-    closeDialog() {
-        this.matDialogRef.close();
-    }
     updateDictionary(dictionary: TitleDescriptionOfDictionary) {
         // TODO a implementer
         console.log(dictionary);
