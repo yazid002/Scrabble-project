37d9d0bff52895703271ea2a07c5e339dc944778
Maxime Laroche <mlaroche2009@gmail.com>
2021-12-02 08:16:26 -0500

    dictionaryOptionsComponent should create réglé

--- client/src/app/components/admin/dictionary-options/dictionary-options.component.spec.ts
+++ client/src/app/components/admin/dictionary-options/dictionary-options.component.spec.ts
@@ -1,15 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { DictionaryOptionsComponent } from './dictionary-options.component';
 
-describe('DictionaryOptionsComponent', () => {
+fdescribe('DictionaryOptionsComponent', () => {
     let component: DictionaryOptionsComponent;
     let fixture: ComponentFixture<DictionaryOptionsComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DictionaryOptionsComponent],
-            imports: [HttpClientTestingModule],
+            imports: [HttpClientTestingModule, MatSnackBarModule],
         }).compileComponents();
     });
 
