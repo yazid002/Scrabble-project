515b410bd4d9429219a83092531db7b6af328c57
Maxime Laroche <mlaroche2009@gmail.com>
2021-12-02 11:17:00 -0500

    add et delete testé

--- server/app/services/dictionary.service.spec.ts
+++ server/app/services/dictionary.service.spec.ts
@@ -1,19 +1,41 @@
-// import { Dictionary } from '@app/classes/dictionary';
-// import { expect } from 'chai';
-// import { createStubInstance, SinonStubbedInstance } from 'sinon';
-// // disable car on importe une constante du coté client, on doit utiliser un pattern
-// // eslint-disable-next-line no-restricted-imports
-// import { DictionaryService } from './dictionary.service';
-// import { ReadFileService } from './read-file.service';
-
-// describe('WordValidationService', () => {
-//     let service: DictionaryService;
-//     let readFileService: SinonStubbedInstance<ReadFileService>;
-//     beforeEach(async () => {
-//         readFileService = createStubInstance(ReadFileService);
-//         readFileService.readDictionary.retur
-//         service = new DictionaryService(readFileService);
-//     });
-
-
-// });
+import * as DictFile from '@app/assets/dictionnary.json';
+import { Dictionary } from '@app/classes/dictionary';
+import { expect } from 'chai';
+import * as fs from 'fs';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
+// disable car on importe une constante du coté client, on doit utiliser un pattern
+// eslint-disable-next-line no-restricted-imports
+import { DictionaryService } from './dictionary.service';
+import { ReadFileService } from './read-file.service';
+const exampleDict: Dictionary = {
+    title: 'new dictionary',
+    description: 'for testing purposes',
+    words: ['aa', 'bb', 'cc'],
+};
+describe('WordValidationService', () => {
+    let service: DictionaryService;
+    let readFileService: SinonStubbedInstance<ReadFileService>;
+    beforeEach(async () => {
+        readFileService = createStubInstance(ReadFileService);
+        readFileService.readDictionary.resolves(DictFile as Dictionary);
+        service = new DictionaryService(readFileService);
+    });
+    describe('addDict', () => {
+        const newDictFileName = 'newDict.json';
+        beforeEach(() => {
+            fs.writeFileSync(service.path + newDictFileName, JSON.stringify(exampleDict));
+        });
+        it('should add the received Dictionary to the dictionaries array and specify that it is not a default dictionary', async () => {
+            readFileService.readDictionary.resolves(exampleDict);
+            await service.addDict(newDictFileName);
+            expect(service.dictionaries).to.contain(exampleDict);
+        });
+    });
+    describe('deleteDictionary', () => {
+        it('should remove the dictionary we want to remove', () => {
+            service.dictionaries.push(exampleDict);
+            service.deleteDictionary(exampleDict.title);
+            expect(service.dictionaries).not.to.contain(exampleDict);
+        });
+    });
+});
