0cb9370d936fe8e77b891806746793ffd332d61f
Maxime Laroche <mlaroche2009@gmail.com>
2021-12-02 09:28:16 -0500

    réglé tests chunk nodes

--- client/src/app/classes/chunk-node.spec.ts
+++ client/src/app/classes/chunk-node.spec.ts
@@ -1,8 +1,13 @@
-import { ChunkNode } from './chunk-node';
-
+// we must disable this import pattern warning because we need a file outside of the project scope (can't use '@app/' pattern)
+// eslint-disable-next-line no-restricted-imports
+import * as dictFile from '../../../../server/app/assets/dictionnary.json';
+import { ChunkNode, setVirtualPlayerDictionary } from './chunk-node';
+import { Dictionary } from './dictionary';
+const dictionary = dictFile as Dictionary;
 describe('ChunkNode', () => {
     it('should create an instance', () => {
         const word = 'allo'.split('');
+        setVirtualPlayerDictionary(dictionary);
         expect(new ChunkNode(word)).toBeTruthy();
     });
 });
